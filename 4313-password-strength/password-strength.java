0692846130314b09a9cd366f0d1c65f0c1601021
Kamini P <kaminiperiyasamy@gmail.com>
2026-05-31 11:50:24 +0530

    Time: 11 ms (85.24%) | Memory: 47.1 MB (67.60%) - LeetSync

---
+++ 4313-password-strength/password-strength.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int passwordStrength(String password) {
+      Set<Character> seen = new HashSet<>();
+        int strength = 0;
+
+        for (char ch : password.toCharArray()) {
+            if (seen.contains(ch)) {
+                continue;
+            }
+
+            seen.add(ch);
+
+            if (ch >= 'a' && ch <= 'z') {
+                strength += 1;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                strength += 2;
+            } else if (ch >= '0' && ch <= '9') {
+                strength += 3;
+            } else if (ch == '!' || ch == '@' || ch == '#' || ch == '$') {
+                strength += 5;
+            }
+        }
+
+        return strength;  
+    }
+}
